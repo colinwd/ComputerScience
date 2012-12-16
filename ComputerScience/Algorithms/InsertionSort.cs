be23d02bdf55a4f18e54043d0d48e2efddf508b4
Colin Davis <cdavis@enviroissues.com>
2012-12-16 12:04:36 -0800

    First commit

---
+++ ComputerScience/Algorithms/InsertionSort.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerScience.Algorithms {
+    class InsertionSort {
+        public static List<T> Sort<T>(List<T> list) where T : IComparable {
+            var ret = new List<T>();
+            for (var i = 0; i < list.Count; i++) {
+                for (var j = i + 1; j <= i + 2; j++) {
+                    if (list[j].CompareTo(list[j-1]) < 0) {
+                        ret.Add(list[j - 1]);
+                        ret.Add(list[j]);
+                    } else {
+                        ret.Add(list[j]);
+                        ret.Add(list[j - 1]);
+                    }
+                }
+            }
+            return ret;
+        }
+    }
+}
---
+++ Tests/AlgorithmsTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ComputerScience.Algorithms;
+
+
+namespace Tests {
+    [TestClass]
+    public class AlgorithmsTests {
+        [TestMethod]
+        public void InsertionSortTest() {
+
+        }
+    }
+}
