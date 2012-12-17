15320c92eaeb1ffaadd7329fb37679c7fbff8e8c
Colin Davis <cdavis@enviroissues.com>
2012-12-17 10:03:12 -0800

    Added stubs for hash tables and binary search trees

---
+++ ComputerScience/Data Structures/BinarySearchTree.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerScience.Data_Structures {
+    /*
+     * Time complexity: O(log n) for insertion, search, and deletion.
+     * 
+     * Maintains strict ordering
+     *      At any node n in the tree with key k, all keys of the elements in n's left subtree are strictly less than k, while all keys of the elemen's in n's right subtree are strictly greater than k.
+     *      
+     * Keys must be unique and comparable.
+     * 
+     * http://www.youtube.com/watch?v=V_3BM0ykITM WATCH THIS
+     * 
+     * http://en.wikipedia.org/wiki/Self-balancing_binary_search_tree#Implementations
+     * 
+     * W
+    */
+    class BinarySearchTree {
+    }
+}
---
+++ ComputerScience/Data Structures/HashTable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerScience.Data_Structures {
+    /*
+     * Allows for O(n) lookup times.
+     * 
+     * Should be implemented without using any built in associative arrays.
+     * 
+     * http://stackoverflow.com/questions/730620/how-does-a-hash-table-work
+     * 
+     * http://en.wikipedia.org/wiki/Hash_table
+     * 
+    */
+    class HashTable {
+    }
+}
