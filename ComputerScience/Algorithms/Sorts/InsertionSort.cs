f23475d9a9f2e5c29ddc5676f9d9fb1f43604728
Colin Davis <cdavis@enviroissues.com>
2013-01-04 17:14:27 -0800

    Did some work!
    Tweaking Insertion sort (still not quite working)
    Merge sort working! All tests pass.

--- ComputerScience/Algorithms/Sorts/InsertionSort.cs
+++ ComputerScience/Algorithms/Sorts/InsertionSort.cs
@@ -4,19 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ComputerScience.Algorithms {
-    class InsertionSort {
-        public static List<T> Sort<T>(List<T> list) where T : IComparable {
-            var ret = new List<T>();
-            for (var i = 0; i < list.Count; i++) {
-                for (var j = i + 1; j <= i + 2; j++) {
-                    if (list[j].CompareTo(list[j-1]) < 0) {
-                        ret.Add(list[j - 1]);
-                        ret.Add(list[j]);
-                    } else {
-                        ret.Add(list[j]);
-                        ret.Add(list[j - 1]);
-                    }
+namespace ComputerScience.Algorithms.Sorts {
+    public class InsertionSort {
+        public static List<int> Sort(List<int> list) {
+            if (list.Count <= 1)
+            {
+                return list;
+            }
+            var ret = new List<int>();
+            for (var i = 1; i < list.Count; i++) {
+                for (var j = i; i > 1 && list[j] < list[j - 1]; j--) {
+                    var tmp = list[j];
+                    list[j] = list[j - 1];
+                    list[j - 1] = tmp;
                 }
             }
             return ret;
---
+++ Tests/InsertionSortTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using ComputerScience.Algorithms.Sorts;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests {
+    [TestClass]
+    public class InsertionSortTests {
+        [TestMethod]
+        public void ZeroLengthListReturnsSameList() {
+            var list = new List<int>();
+            Assert.AreEqual(list, InsertionSort.Sort(list));
+        }
+
+        [TestMethod]
+        public void OneLengthListReturnsSameList() {
+            var list = new List<int> { 1 };
+            Assert.AreEqual(list, InsertionSort.Sort(list));
+        }
+
+        [TestMethod]
+        public void ProperlySortsLists() {
+            var list1 = new List<int> { 2, 7, 8, 3, 1 };
+            var list2 = new List<int> { 4, 2, 1, 1, 1 };
+
+            var list1s = new List<int> { 1, 2, 3, 7, 8 };
+            var list2s = new List<int> { 1, 1, 1, 2, 4 };
+
+            CollectionAssert.AreEqual(InsertionSort.Sort(list1), list1s);
+            CollectionAssert.AreEqual(InsertionSort.Sort(list2), list2s);
+        }
+    }
+}
