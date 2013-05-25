035799e825bf5ee66921796a14b209e1430847c3
Colin <davis.colin.w@gmail.com>
2013-05-25 15:21:17 -0700

    bubble sort implemented and green

--- ComputerScience/Algorithms/Sorts/BubbleSort.cs
+++ ComputerScience/Algorithms/Sorts/BubbleSort.cs
@@ -4,7 +4,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ComputerScience.Algorithms {
-    class BubbleSort {
+namespace ComputerScience.Algorithms.Sorts
+{
+    public class BubbleSort
+    {
+        /// <summary>
+        /// int[] implementation of bubble sort
+        /// O(n^2), even in the average case. Best case is O(n) with an already sorted list, which is an advantage of bubble and insertion sort.
+        /// </summary>
+        /// <param name="list"></param>
+        /// <returns></returns>
+        public static int[] Sort(int[] list)
+        {
+            if (list.Length <= 1)
+            {
+                return list;
+            }
+
+            var ret = new int[list.Length];
+            Array.Copy(list, ret, list.Length);
+            bool swapped = false;
+
+            do
+            {
+                swapped = false;
+                for (var i = 1; i < list.Length; i++)
+                {
+                    if (ret[i] < ret[i - 1])
+                    {
+                        int tmp = ret[i];
+                        ret[i] = ret[i - 1];
+                        ret[i - 1] = tmp;
+                        swapped = true;
+                    }
+                }
+            } while (swapped);
+
+            return ret;
+        }
     }
 }
---
+++ Tests/BubbleSortTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using ComputerScience.Algorithms.Sorts;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class BubbleSortTests
+    {
+        [TestMethod]
+        public void ZeroLengthListReturnsSameList()
+        {
+            var list = new int[0];
+            Assert.AreEqual(list, BubbleSort.Sort(list));
+        }
+
+        [TestMethod]
+        public void OneLengthListReturnsSameList()
+        {
+            var list = new int[1];
+            list[0] = 1;
+            Assert.AreEqual(list, BubbleSort.Sort(list));
+        }
+
+        [TestMethod]
+        public void ProperlySortsLists()
+        {
+            int[] list1 = new List<int> { 2, 7, 8, 3, 1 }.ToArray();
+            int[] list2 = new List<int> { 4, 2, 1, 1, 1 }.ToArray();
+
+            int[] list1s = new List<int> { 1, 2, 3, 7, 8 }.ToArray();
+            int[] list2s = new List<int> { 1, 1, 1, 2, 4 }.ToArray();
+
+            CollectionAssert.AreEqual(BubbleSort.Sort(list1), list1s);
+            CollectionAssert.AreEqual(BubbleSort.Sort(list2), list2s);
+        }
+    }
+}
